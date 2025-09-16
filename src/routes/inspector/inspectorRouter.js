9bb8fc30e51fb24567e2f4433818077fdcd036a8
Aswin Krishna <aswinkrishna005@gmail.com>
2025-09-16 07:36:14 +0530

    changing the cloudinary folder for inspector

--- src/routes/inspector/inspectorRouter.js
+++ src/routes/inspector/inspectorRouter.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const createUploader = require("../../middleware/upload");
 const {
   getAvailableEnquiries,
   placeBid,
   cancelBid,
   getMyBids,
-  getLowestBidsPerEnquiry
+  getLowestBidsPerEnquiry,
+  updateInspectorDocumentsController
 } = require("../../controllers/inspector/inspectorController");
 const verifyUser = require("../../middleware/verifyUser");
+
+const inspectorUploader = createUploader("inspectors");
 
 router.get("/enquiries", verifyUser, getAvailableEnquiries);
 router.post("/bid/:enquiryId", verifyUser, placeBid);
 router.delete("/bid/:bidId", verifyUser, cancelBid);
 router.get("/my-bids", verifyUser, getMyBids);
 router.get("/lowest-bids", verifyUser, getLowestBidsPerEnquiry);
+router.patch(
+  "/profile/updateDocuments",
+  verifyUser,
+  inspectorUploader.fields([
+    { name: "aadhaarCard", maxCount: 1 }
+  ]),
+  updateInspectorDocumentsController
+);
+
 
 module.exports = router;
